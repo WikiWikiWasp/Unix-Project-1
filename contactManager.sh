2ffb65da52be10a2ddec14928cf1950d34820c69
Mark McDermott <markmcdermott@Marks-MacBook-Pro.local>
2017-02-13 20:39:26 -0600

    #1 added menu display

--- contactManager.sh
+++ contactManager.sh
@@ -6,21 +6,53 @@
 
 # Defining Find() function
 Find() {
+  echo "stub"
 }
 # Defining Add() function
 Add() {
+  echo "stub"
 }
 # Defining Update() function
 Update() {
+  echo "stub"
 }
 # Defining Remove() function
 Remove() {
+  echo "stub"
 }
 # Defining Display() function
 Display() {
+  echo "stub"
 }
 # Defining Quit() function
 Quit() {
+  echo "stub"
 }
 # Main script
 
+PS3='Welcome to Contact Manager.  Please enter a letter: '
+optionlist=("
+(a) Find a record
+(b) Update a record
+(c) Remove a record
+(d) Remove a record
+(e) Quit")
+select option in "${optionlist[@]}"
+do
+    case $option in
+        "a")
+            echo "Find a record"
+            ;;
+        "b")
+            echo "Update a record"
+            ;;
+        "c")
+            echo "Remove a record"
+            ;;
+        "d")
+            echo "Quit"
+            break
+            ;;
+        *) echo not a valid option;;
+    esac
+done
