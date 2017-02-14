e094e96e2dbd4265e952d3735a1d0c6115150200
WikiWikiWasp <jason_flinn@homedepot.com>
2017-02-14 14:31:30 -0600

    #3 - user input prompts, begin verification

--- contactManager.sh
+++ contactManager.sh
@@ -10,7 +10,92 @@ Find() {
 }
 # Defining Add() function
 Add() {
-  echo "add stub"
+    #echo "add stub"
+    #User prompts and inputs
+    echo "Please enter contact Name: "
+    read $CONNAME
+    #check if input < 1
+    if [ ${#CONNAME} -lt 1 ]
+    then
+        do 
+            echo "No input submitted, Please enter contact name again: "
+            read $CONNAME
+            #keep looping until they enter a value
+            if [ ${#CONNAME} -lt 1 ]
+            then
+                continue
+            else
+                break
+            fi
+        done
+    fi
+    echo "Please enter contact Address: "
+    read $CONADD
+    #check if input < 1
+    if [ ${#CONADD} -lt 1 ]
+    then
+        do 
+            echo "No input submitted, Please enter contact address again: "
+            read $CONADD
+            #keep looping until they enter a value
+            if [ ${#CONADD} -lt 1 ]
+            then
+                continue
+            else
+                break
+            fi
+        done
+    fi
+    echo "Please enter contact Phone Number (XXXXXXX): "
+    read $CONPHONE
+    #check if input < 1
+    if [ ${#CONPHONE} -lt 1 ]
+    then
+        do 
+            echo "No input submitted, Please enter contact phone number again: "
+            read $CONPHONEE
+            #keep looping until they enter a value
+            if [ ${#CONPHONE} -lt 1 ]
+            then
+                continue
+            else
+                break
+            fi
+        done
+    fi
+    if [ ${#CONPHONE} -gt 7 ]
+    then
+        do
+            echo "Too many digits entered, please enter contact phone number again with 7 digits in the format XXXXXXX: "
+            read $CONPHONE
+            if [ ${#CONPHONE} -gt 7 ]
+            then
+                continue
+            else
+                break
+            fi
+        done
+    fi
+    echo "Please enter contact Email: "
+    read $CONEMAIL
+    #check if input < 1
+    if [ ${#CONEMAIL} -lt 1 ]
+    then
+        do 
+            echo "No input submitted, Please enter contact email again: "
+            read $CONEMAIL
+            #keep looping until they enter a value
+            if [ ${#CONEMAIL} -lt 1 ]
+            then
+                continue
+            else
+                break
+            fi
+        done
+    fi
+
+    #check if input > 0
+    if []
 }
 # Defining Update() function
 Update() {
