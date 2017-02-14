9c3135712fde1695c5a97ea79d8f0a38b2d799b4
Car234 <car234@txstate.edu>
2017-02-13 20:24:13 -0600

    Created Skeleton to .sh file

--- contactManager.sh
+++ contactManager.sh
@@ -1,4 +1,27 @@
+#!/bin/bash
 # Mark McDermott
 # Jason Flinn
 # Carlo Rodriguez
 # Benjamin Winston
+
+# Defining Find() function
+Find() {
+}
+
+# Defining Add() function
+Add() {
+}
+# Defining Update() function
+Update() {
+}
+# Defining Remove() function
+Remove() {
+}
+# Defining Display() function
+Display() {
+}
+# Defining Quit() function
+Quit() {
+}
+# Main script
+
